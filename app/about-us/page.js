b1940a99d862099cb6fa13fb8960e817534b8d1d
Kate Meuse <38730033+cyankaet@users.noreply.github.com>
2026-01-08 14:07:24 -0800

    Update page.js

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     <TestResult passed={true} title="AppDir Static Page">
       <p>
         If you see this page, the app dir is working properly along with the
-        pages router. I have made a change you really don't know about.
+        pages router. I have made a change you really shouldn't know about.
       </p>
     </TestResult>
   );
